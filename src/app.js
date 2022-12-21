4cc926c08d13f9627d46f7ce39cbde2ed7b6a54b
sduartecor <santiagoduartecor@outlook.com>
2022-12-21 16:41:31 +0000

    Primera version

--- src/app.js
+++ src/app.js
@@ -7,5 +7,48 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  document.querySelector("#bttnDomain").addEventListener("click", () => {
+    document.querySelector("#domainText").innerHTML = generateExcuse();
+  });
+};
+
+let generateExcuse = () => {
+  var pronoun = ["the", "our"];
+  var adj = ["great", "big"];
+  var noun = ["jogger", "racoon"];
+  var extension = ["com", "net", "edu", "uy", "org", "es"];
+
+  var result = "";
+  //Forma 1
+  for (let value1 of pronoun) {
+    for (let value2 of adj) {
+      for (let value3 of noun) {
+        for (let value4 of extension) {
+          result += "<li>" + value1 + value2 + value3 + "." + value4 + "</li>";
+        }
+      }
+    }
+  }
+
+  //Forma 2
+  /*
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let j = 0; j < adj.length; j++) {
+       for (let k = 0; k < noun.length; k++) {
+         for (let e = 0; e < extension.length; e++) {
+           result +=
+             "<li>" +
+             pronoun[i] +
+             adj[j] +
+             noun[k] +
+             "." +
+             extension[e] +
+             "</li>";
+         }
+       }
+     }
+   }
+   */
+
+  return result;
 };
